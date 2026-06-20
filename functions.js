21ed155644f3d4638707761bc1f1c3c97503c9d6
Ganeshdvp <ganeshcherupalli6565@gmail.com>
2026-06-20 12:09:41 +0530

    Solved few problems

--- functions.js
+++ functions.js
@@ -5,14 +5,12 @@
 // var sum = add(45,67);
 // console.log(sum);
 
-
 // 2- Write a function which can return the greatest of the three numbers passed in arguments?
 // function greatestNum(a,b,c){
 //     return a > b ? (a>c ? a : c) : (b>c ? b : c)
 // }
 // var res = greatestNum(10,20,30);
 // console.log(res);
-
 
 // 3- Write a function which can print the given arguments in ascending order?
 // function sortNums(a,b,c){
@@ -55,11 +53,10 @@
 //         }
 //     }
 //     return [first,second,third]
-    
+
 // }
 // var res = sortNums(10,5,20);
 // console.log(res);
-
 
 // 4- Write a function which can return the factorial of a number.
 // function fact(a){
@@ -68,7 +65,6 @@
 // }
 // var factorial = fact(4);
 // console.log(factorial);
-
 
 // 5- Write a function which can to return the reverse of a given number.
 // function rev(a){
@@ -84,11 +80,10 @@
 // var reverse = rev(1234);
 // console.log(reverse);
 
-
 // 6- Write a function which can check a given number is prime number or not?
 // function prime(num){
 //     if(num<=1) return console.log("it is not prime");
-    
+
 //     for(let i=2; i<num; i++){
 //         if(num%i === 0){
 //             console.log("it is not prime");
@@ -98,7 +93,6 @@
 //     console.log("it is prime");
 // }
 // prime(7)
-
 
 // 7- Write a function which can check a given number is palindrome or not?
 // function palindrome(num){
@@ -120,7 +114,6 @@
 // }
 // palindrome(121);
 
-
 // 8- Write a function to return the number of digits in the given number?
 // function digits(num){
 //     let count = 0;
@@ -135,7 +128,6 @@
 // var res = digits(125);
 // console.log(res);
 
-
 // 9- Write a function that prints all even numbers between 1 and 20.?
 // function even(){
 //     let start = 1;
@@ -149,14 +141,12 @@
 // }
 // even();
 
-
 // 10- Write a function which can add two numbers?
 // function add(a,b){
 //     return a+b
 // }
 // const res = add(5,9);
 // console.log(res);
-
 
 // 11- Write a function which can display the greatest of the three numbers passed in arguments.?
 // function greatest(a,b,c){
@@ -170,14 +160,12 @@
 // const res = greatest(500,15,200)
 // console.log(res);
 
-
 // 12- Write a function which can display the given arguments in ascending order?
 // function sort(...args){
 //     return [...args].sort((i,j)=> i-j)
 // }
 // const res = sort(10,5,20,4);
 // console.log(res);
-
 
 // 13- Write a function which can display the factorial of a number?
 // function factorial(n){
@@ -186,7 +174,6 @@
 // }
 // const res = factorial(4);
 // console.log(res);
-
 
 // 14- Write a function which can to display the reverse of a given number?
 // function rev(num){
@@ -201,7 +188,6 @@
 // }
 // const res = rev(123);
 // console.log(res);
-
 
 // 15- Write a function which can check a given number is armstrong number or not?
 // function armstrong(num){
@@ -229,7 +215,6 @@
 // }
 // armstrong(153);
 
-
 // 16- Write a function which can return the factorial of a number to another variable?
 // function factorial(n){
 //     let fact = 1;
@@ -242,7 +227,6 @@
 // }
 // const res = factorial(4);
 // console.log(res);
-
 
 // 17- Write a function that returns an array of all even numbers between 1 and 20?
 // function even(num){
@@ -257,14 +241,12 @@
 // const res = even(1234);
 // console.log(res);
 
-
 // 18- Cube of any number using the function?
 // function cube(num){
 //     return Math.pow(num,3)
 // }
 // const res = cube(4);
 // console.log(res);
-
 
 // 19- Find diameter, circumference and area of a circle using functions?
 // let radius = 5;
@@ -281,7 +263,6 @@
 // console.log(circumference(radius));
 // console.log(area(radius));
 
-
 // 20- Maximum and minimum between two numbers using functions.?
 // function numbers(a,b){
 //     let max = a > b ? a : b;
@@ -291,7 +272,6 @@
 //     console.log("Minimum =", min);
 // }
 // numbers(10,20)
-
 
 // 21- Check whether a number is even or odd using functions?
 // function check(num){
@@ -305,12 +285,11 @@
 // }
 // check(0);
 
-
 // 22- Check whether a number is prime, Armstrong or perfect number using functions?
 // let num = 10;
 // function prime(num){
 //     if(num <= 1) return console.log("it is not prime");
-    
+
 //     for(let i=2; i<num; i++){
 //         if(num%i === 0){
 //             console.log("it it not prime");
@@ -336,7 +315,7 @@
 //         temp = Math.floor(temp/10);
 //     }
 //     if(orignalNum === sum){
-//         console.log("it is armstorng");   
+//         console.log("it is armstorng");
 //     }
 //     else{
 //         console.log("it is not armstorng");
@@ -362,11 +341,10 @@
 // armstrong(num);
 // perfectNum(num);
 
-
 // 23- Find all prime numbers between the given interval using functions?
 // function isPrime(num){
 //     if(num <= 1) return false;
-    
+
 //     for(let i = 2; i<num; i++){
 //         if(num%i === 0){
 //             return false;
@@ -382,7 +360,6 @@
 //     }
 // }
 // printPrimes(10,50);
-
 
 // 24- check whether a given number is strong number or not?
 // function factorial(num){
@@ -410,7 +387,6 @@
 // }
 // sum(145);
 
-
 // 25- Print all strong numbers between the given interval using functions?
 // function factorial(num){
 //     let fact = 1;
@@ -437,7 +413,6 @@
 // }
 // sum(1,146);
 
-
 // 26- Armstrong numbers between the given interval using functions?
 // function countNums(num){
 //     let count = 0;
@@ -460,11 +435,10 @@
 //         if(sum === i){
 //             console.log(i);
 //         }
-        
+
 //     }
 // }
 // armstorng(1,1000);
-
 
 // 27- Print all perfect numbers between the given interval using functions?
 // function perfectNums(start,end){
@@ -482,14 +456,12 @@
 // }
 // perfectNums(1,100);
 
-
 // 28- Find the power of any number using recursion?
 // function powerFn(num,power){
 //     if(power === 0) return 1;
 //     return num * powerFn(num, power-1)
 // }
 // console.log(powerFn(4,2));
-
 
 // 29- Print all natural numbers between 1 to n using recursion?
 // function naturalNums(start, end){
@@ -498,7 +470,6 @@
 //     return naturalNums(start+1,end)
 // }
 // naturalNums(1,10);
-
 
 // 30- Print all even or odd numbers in a given range using recursion?
 // function even(start,end){
@@ -512,7 +483,6 @@
 // }
 // even(1,10);
 
-
 // 31- Sum of all natural numbers between 1 to n using recursion?
 // function naturalNums(start,end){
 //     if(start === end) return end;
@@ -520,10 +490,59 @@
 // }
 // console.log(naturalNums(1,10));
 
-
 // 32- Find the sum of all even or odd numbers in a given range using recursion?
 // function evenSum(start,end){
 //     if(start > end) return 0;
 //     return start + evenSum(start+2,end)
 // }
 // console.log(evenSum(2,10));
+
+// 33- Find reverse of any number using recursion.
+// function rev(num, reverse = 0){
+//     if(num === 0) return reverse;
+//     reverse = reverse * 10 + (num%10);
+//     return rev(Math.floor(num/10), reverse);
+// }
+// console.log(rev(123));
+
+// 34- Check whether a number is a palindrome or not using recursion?
+// let originalNum = 121;
+// function palindrome(num, rev = 0) {
+//   if (num === 0) return rev;
+//   rev = rev * 10 + (num % 10);
+//   return palindrome(Math.floor(num / 10), rev);
+// }
+// const res = palindrome(originalNum);
+// if (originalNum === res) {
+//   console.log("it is palindrome");
+// } else {
+//   console.log("it is not palindrome");
+// }
+
+
+// 35- Find the sum of digits of a given number using recursion.
+// function sum(num){
+//     if(num === 0) return 0;
+//     return num%10 + sum(Math.floor(num/10));
+// }
+// console.log(sum(1234));
+
+
+// 36- Find factorial of any number using recursion?
+// function factorial(num){
+//     if(num === 1) return 1;
+//     return num * factorial(num-1);
+// }
+// console.log(factorial(4));
+
+
+// 37- Generate nth Fibonacci term using recursion?
+// function fibo(num){
+//     if(num === 0) return 0;
+//     if(num === 1) return 1;
+//     return fibo(num-1) + fibo(num-2);
+// }
+// console.log(fibo(10));
+
+
+// 38- Find gcd (HCF) of two numbers using recursion?
